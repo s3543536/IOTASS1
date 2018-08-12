1dbf46fafafe581c23cfb69bebafe8def33b356d
s3543536 <s3543536@student.rmit.edu.au>
2018-08-12 23:56:49 +0000

    On branch cronjob
    	new file:   cron.py
    		for now, runs the log_pressure script once every minute

---
+++ cron.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from crontab import CronTab
+
+#init cron
+cron = CronTab(user=True)
+cron.remove_all()
+
+#add a new job
+job = cron.new(command='/home/pi/Assignment1/log_pressure.py')
+
+#configure job
+job.minute.every(1)
+cron.write()
