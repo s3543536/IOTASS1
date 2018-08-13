29df9f4d040d03b105a975af43c05aaa415b0934
1wsx10 <1wsx10@gmail.com>
2018-08-13 22:14:10 +0000

    On branch cronjob
    	modified:   cron.py
    		changed the python path to be the real one

--- cron.py
+++ cron.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 from crontab import CronTab
 
 #init cron
@@ -6,8 +6,9 @@ cron = CronTab(user='pi')
 cron.remove_all()
 
 #add a new job
-job = cron.new(command='/home/pi/Assignment1/log_pressure.py')
+job = cron.new(command='/usr/bin/python3 /home/pi/IOTASS1/log_pressure.py')
 
 #configure job
-job.minute.every(1)
+job.hour.every(1)
+
 cron.write()
